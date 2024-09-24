a4bb85ae6499ca824808f9bb09f836234cbce9b6
Kaiden Richardson <kaidenr@shaw.ca>
2024-09-24 12:09:23 -0700

    Added a preprocessor script to fix the wiki generated links. (#5)
    * Added a preprocessor script to fix the wiki generated links.
    
    * Try to fix relative path issue.
    
    * Start debugging with the fix script
    
    * Fix entire build folder.
    
    * Restore wiki generation to be on push to main only.
    
    * Fixed linter issues

--- dcc_ex_py/DCCEX.py
+++ dcc_ex_py/DCCEX.py
@@ -21,9 +21,7 @@ class DCCEX:
         """Create a new connection to a DCC-EX Server
 
         :param ip: The (local) ip address of the server to connect to. If you haven't set this, it is probably '192.168.4.1'
-        :type ip: str
         :param port: The numeric port to connect on, usually 2560.
-        :type port: int
         """
         # Internal prep
         self._init_sockets(ip, port)
@@ -49,10 +47,14 @@ class DCCEX:
                 listener(decodedMsg)
 
     def _init_listener(self) -> None:
+        """Internal function to start the listener thread.
+        """
         self.listener_thread: threading.Thread = threading.Thread(target=self._listener, daemon=True)
         self.listener_thread.start()
 
     def _init_sockets(self, ip: str, port: int) -> None:
+        """Internal function to create socket objects and connect to the server.
+        """
         self.ip: str = ip
         self.port: int = port
 
@@ -60,11 +62,24 @@ class DCCEX:
         self.client_socket.connect((ip, port))
 
     def send_command(self, command: str) -> None:
+        """Send a string command to the DCC-EX Controller. Note that the command is not validated, a newline is added to the end of it though.
+        For pre-formatted commands, consider using one of the available helper classes instead.
+
+        :param command: The string command to be sent to DCC-EX
+        """
         command += '\n'
         self.client_socket.sendall(command.encode())
 
     def add_command_listener(self, callback: Callable[[DecodedCommand], None]) -> None:
+        """Register a callback function to be called if we receive input from the DCC-EX Controller.
+
+        :param callback: The function to be called, it is passed a DecodedCommand which contains all the relavent information. It is not required to return anything.
+        """
         self.onPacketReceived.append(callback)
 
     def remove_command_listener(self, callback: Callable[[DecodedCommand], None]) -> None:
+        """Remove a callback function from the list of callbacks called when a packet is received.
+
+        :param callback: The callback function to remove.
+        """
         self.onPacketReceived.remove(callback)
