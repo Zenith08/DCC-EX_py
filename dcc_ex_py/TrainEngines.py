77a862db5c03eee96b469e7d9e7785bfda84900f
Kaiden Richardson <kaidenr@shaw.ca>
2024-10-30 17:51:49 -0700

    Added train engine handling code.

--- dcc_ex_py/TrainEngines.py
+++ dcc_ex_py/TrainEngines.py
@@ -3,6 +3,75 @@ from typing import Any
 from .Helpers import DecodedCommand, Direction, ActiveState
 
 
+class TrainEngine:
+    """Represents an engine currently active on the track that the command station knows about.
+    """
+
+    def __init__(self, trainEngines: Any, cab: int) -> None:
+        """## Internal Function, creating a new Train Engine has no meaning if called outside of the API.
+        Instantiates a Train Engine when we first learn about it because the command station told us about it.
+
+        Train Engines are not created when we first define them and are only created by broadcasts from the command staiton.
+
+        :param trainEngines: The local TrainEngines that owns this train. Used for sending commands out from this object.
+        :param cab: The DCC address of this locomotive (Locomotive and Accessory addresses are independent).
+        """
+
+        self.trainEngines: TrainEngines = trainEngines
+        """An internal reference to our own controls so they can be controlled from this local instance."""
+        self.cab: int = cab
+        """The id of the train engine being controlled."""
+        self.speed: int = 0
+        """The speed of the engine without direction (0 - 126)."""
+        self.direction: Direction = Direction.FORWARD
+        """The direction of the engine."""
+
+        self.functions: int = 0
+        """An int where each bit represents a given function and whether it is on or off. Parse with get_function"""
+
+    def get_function(self, function: int) -> ActiveState:
+        """Checks if a given function is active or not on the locomotive.
+
+        :param function: The function on the train to check (0-31).
+
+        :returns: ActiveState.ON if the given function is active, otherwise ActiveState.OFF
+        """
+        if (self.functions >> function) & 0b1:
+            return ActiveState.ON
+        else:
+            return ActiveState.OFF
+
+    def _information_received(self, speedDir: int, func: int) -> None:
+        """An internal notificaiton to update the train engine information based on information received from the broadcast.
+
+        :param speed: The speed of the engine where bits 0-6 are speed from 1-128 and bit 7 is direction.
+        :param func: The bit pattern for activated functions."""
+        self.speed = speedDir & 0b1111111
+
+        if speedDir & 0b10000000 != 0:
+            self.direction = Direction.FORWARD
+        else:
+            self.direction = Direction.REVERSED
+
+        self.functions = func
+
+    def set_speed(self, speed: int, direction: Direction) -> None:
+        """Sets the train engine's speed and direction. Does not update the local values, they are only updated from broadcasts.
+
+        :param speed: The new speed to set the train to (0-126), -1 is emergency stop.
+        :param direction: The direction to move the train in.
+        """
+        self.trainEngines.set_speed(self.cab, speed, direction)
+
+    def set_function(self, function: int, active: ActiveState) -> None:
+        """Sets the target function on or off for this train. Does not update the local value, they are only updated from broadcasts.
+
+        :param function: The function to adjust (0-31).
+        :param active: Whether to turn the function on or off. You must manually turn off functions that should not be latched.
+        """
+        self.trainEngines.set_cab_function(self.cab, function, active)
+
+
 class TrainEngines:
     """Wraps control of DCC-EX Cab Control and handles feedback.
     """
@@ -15,7 +84,8 @@ class TrainEngines:
         from .DCCEX import DCCEX
         self.controller: DCCEX = controller
 
-        self.engines: list[ActiveEngine] = []
+        self.engines: dict[int, TrainEngine] = {}
+        """An internal mapping of cab ids to engines that can be controlled."""
 
         self.maxEngines: int = 0  # init unknown
         """The max number of engines the command station supports at the same time based on onboard memory (0 if not checked, doesn't include power limitations)."""
@@ -43,6 +113,12 @@ class TrainEngines:
         """
         self.controller.send_command("<->")
 
+    def check_max_engines(self) -> None:
+        """Refreshes the information on how many locomotives the command station supports.
+        The information will not be available right away, it comes from the return value from the server.
+        """
+        self.controller.send_command("<#>")
+
     def emergency_stop(self) -> None:
         """Emergency stops all trains, leaves track power on.
         """
@@ -60,23 +136,15 @@ class TrainEngines:
     def _command_received(self, command: DecodedCommand) -> None:
         """Internal listener to catch changes on the command station both caused by this program and other connections.
 
+        Note we discard T commands because they don't provide information on which loco they affect.
+
         :param command: The command we received after parsing it into a helper class.
         """
-        # TODO treat:
-        # T
-        # '#' command, also needs outbound function
-        # 'l' (idk if this is new or old function system)
-        pass
-
-
-class ActiveEngine:
-    """Represents an engine currently active on the track that the command station knows about.
-    TODO
-    """
-
-    def __init__(self, cab: int) -> None:
-        self.cab: int = cab
-        self.speed: int = 0
-        self.forward: Direction = Direction.FORWARD
-
-        # TODO functions don't seem to have a return value so the are ignored here
+        if command.command == 'l':
+            cab: int = int(command.args[0])
+            engine: TrainEngine = self.engines.get(cab, TrainEngine(self, cab))
+            if cab not in self.engines:
+                self.engines[cab] = engine
+            engine._information_received(int(command.args[2]), int(command.args[3]))
+        elif command.command == '#':
+            self.maxEngines = int(command.args[0])
