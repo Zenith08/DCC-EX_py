c9d0919f519883fa7dc164e53dfed032af7cbd20
Kaiden Richardson <kaidenr@shaw.ca>
2025-01-11 22:51:25 -0800

    Preliminary implementation of live tests.

--- live-tests/test_LiveTrain.py
+++ live-tests/test_LiveTrain.py
@@ -1,7 +1,8 @@
 """A test system called by Github Actions separately to run tests on real hardware."""
+import sys
+import threading
 from threading import Event
 from dcc_ex_py.DCCEX import DCCEX
-from dcc_ex_py.CVProgrammer import CVProgrammer
 
 # Globals
 commandStation: DCCEX = DCCEX("10.0.0.25", 2560)
@@ -10,7 +11,34 @@ errorMessages: list[str] = []
 
 def test_live_basics():
     """Basic test to see if the decoder is responding."""
+    global errorMessages
+
+    test_complete: Event = threading.Event()
+
+    def local_callback(cv: int, value: int):
+        if cv != 8:
+            errorMessages.append(f"test_live_basics: Expected CV 8, got CV {cv}")
+        if value != 129:
+            errorMessages.append(f"test_live_basics: Expected to read 129 (Digitrax ID), got {value}")
+        # Else pass
+        test_complete.set()
+    
+    commandStation.programming.read_cv(8, local_callback)
+    if not test_complete.wait(10):
+        errorMessages.append("test_live_basics: Code timed out when reading CV.")
 
 
 if __name__ == "__main__":
-    pass
+    # Do all tests
+    test_live_basics()
+
+    # Process results
+    if len(errorMessages) == 0:
+        print("All tests passed, success.")
+        sys.exit(0)
+    else:
+        print("Test failures occurred:")
+        for message in errorMessages:
+            print(f"    {message}")
+        print("End of errors.")
+        sys.exit(len(errorMessages))
